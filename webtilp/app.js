8dc872985ac81b3fa2e75d86a92f99e66edadc5f
Adrien Bertrand <bertrand.adrien@gmail.com>
2026-01-28 18:47:57 +0100

    WebTILP: we don't need to call silentReconnectAfterNspireTransfer now.

--- webtilp/app.js
+++ webtilp/app.js
@@ -4282,9 +4282,6 @@ async function receiveSelected() {
                 log(`Failed to receive ${entry.name} (${formatErrorResult(module, result)}).`);
             }
         }
-        if (receivedAny) {
-            await silentReconnectAfterNspireTransfer();
-        }
     } catch (err) {
         logError(err, 'Receive selected failed');
     } finally {
