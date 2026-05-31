db609f9df15394a9cdf3200cb3df047fd68e91e4
Adrien Bertrand <bertrand.adrien@gmail.com>
2026-05-31 20:29:39 +0200

    WebTILP: handle missing active model label.

--- webtilp/app.js
+++ webtilp/app.js
@@ -2995,7 +2995,7 @@ function getActiveCalcModelString() {
 }
 
 function resolveDeviceModelName(infoProductName) {
-    let calcModelLabel = getActiveCalcModelString();
+    let calcModelLabel = getActiveCalcModelString() || '';
     if (hasSilverlinkConnected()) {
         const calcModel = state.settings?.calcModel ?? 'auto';
         if (calcModel !== 'auto') {
