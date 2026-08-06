dd2ae429e34307ed39bd47d7a24d0bda0aaa46cc
Adrien Bertrand <bertrand.adrien@gmail.com>
2026-08-06 20:10:33 +0200

    libhpcalcs: import upstream HPLP repo from debrouxl.
    commit 7b07ea9790ef253aafa39bf75d994fb996282048 under hplp/.

---
+++ hplp/recompile_hplp.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+# Maintainer script for automating the compilation and installation of hplp
+# from a checkout of the complete hplp repository over at Github.
+# Simplified from the tilp / gfm script (recompile_tilp.sh).
+#
+# Copyright (C) 2010, 2011, 2012, 2013, 2014, 2015 Lionel Debroux
+
+# The prefix where the binaries will be installed, e.g. $HOME, /usr, /usr/local.
+PREFIX="$HOME"
+
+# Common flags and definitions.
+CCFLAGS="-fstack-protector-all --param=ssp-buffer-size=1 -fsanitize=undefined,bounds" # This set of flags is intentionally small, the bulk is set by configure.ac.
+CCPPFLAGS="-D_FORTIFY_SOURCE=2"
+
+# Configure and build the given module
+handle_one_module() {
+  module_name="$1"
+  shift # Kick the first argument, so as to be able to pass the rest to configure.
+
+  cd "$module_name"
+  echo "Configuring $module_name"
+  rm -f config.cache
+  ./configure CPPFLAGS="$CCPPFLAGS" CFLAGS="$CCFLAGS $CFLAGS" CXXFLAGS="$CCFLAGS $CFLAGS" --prefix="$PREFIX" $@ || return 1
+  echo "Building $module_name"
+  make clean || return 1
+  make || return 1
+  echo "Installing $module_name"
+  make check || return 1
+  make install || return 1
+  cd -
+}
+
+if [ "x$NOAUTORECONF" = "x" -a "x$NO_AUTORECONF" = "x" ]; then
+echo "=== AUTORECONF ==="
+sh run_autoreconf.sh
+fi
+
+echo "=== UPDATEPOT ==="
+sh run_updatepot.sh
+
+
+echo "=== libhpcalcs ==="
+handle_one_module libhpcalcs || exit 1
